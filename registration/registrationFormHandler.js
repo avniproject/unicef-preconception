95b513b140470ed9ea1cc887d4cab2f8695886ae
nupoorkhandelwal <nupoor.khadelwal@gmail.com>
2019-04-29 13:50:55 +0530

    adding counselling points in registration

--- registration/registrationFormHandler.js
+++ registration/registrationFormHandler.js
@@ -1,14 +1,16 @@
 import lib from '../lib';
+const _ = require("lodash");
+const moment = require("moment");
 import {
     StatusBuilderAnnotationFactory, 
-    RuleFactory, 
+    RuleFactory,  
     FormElementsStatusHelper,
     complicationsBuilder as ComplicationsBuilder
 } from 'rules-config/rules';
 
 const WithRegistrationStatusBuilder = StatusBuilderAnnotationFactory('individual', 'formElement');
 const RegistrationViewFilter = RuleFactory("173abccc-2c3e-451e-a77e-524b28c4a224", "ViewFilter");
-const RegistrationDecisionRule = RuleFactory("173abccc-2c3e-451e-a77e-524b28c4a224", "Decision");
+//const RegistrationDecisionRule = RuleFactory("173abccc-2c3e-451e-a77e-524b28c4a224", "Decision");
 
 
 
@@ -17,38 +19,61 @@ class PreconceptionRegistrationViewHandler {
     static exec(individual, formElementGroup) {
         return FormElementsStatusHelper
             .getFormElementsStatusesWithoutDefaults(new PreconceptionRegistrationViewHandler(), individual, formElementGroup);
-    }
+    }    
 
+   
+    @WithRegistrationStatusBuilder
+    counselHerToDelayPregnancyByUsingContraceptivesAsAgeIsLessThan20([], statusBuilder) {
+        statusBuilder.show().when.ageInYears.is.lessThan(20);
+        }
+
+    @WithRegistrationStatusBuilder
+    counselHerToAsAgeIsGreaterThan30([], statusBuilder) {
+        statusBuilder.show().when.ageInYears.is.greaterThan(30);
+        }
+
+    @WithRegistrationStatusBuilder
+    counselHerAboutEffectsOfConsanguenousMarrriageAndCongenitalAnomalies([], statusBuilder) {
+        statusBuilder.show().when.valueInRegistration("Did you marry within relation?").is.yes;
+        }
+}
+
+// @RegistrationDecisionRule("1b29c5b0-0080-4124-b137-898f6ad3d0d6", "Preconception Registration Decision", 100.0, {})
+// class PreconceptionRegistrationDecisions {
+//     static consanguineousMarraige(individual) {        
+//         const complicationsBuilder = new ComplicationsBuilder({
+//             individual: individual,
+//             complicationsConcept: 'High Risk Conditions'
+//         });
     
-}
+//         complicationsBuilder.addComplication("Consanguineous marriage")
+//         .when.valueInRegistration("Did you marry within relation?")
+//         .containsAnswerConceptName("Yes");
 
-@RegistrationDecisionRule("1b29c5b0-0080-4124-b137-898f6ad3d0d6", "Preconception Decision", 100.0, {})
-class PreconceptionRegistrationDecisions {
-    static exec(programEncounter, decisions, context, today) {
-        const highRiskBuilder = new ComplicationsBuilder({
-            programEnrolment: programEncounter.programEnrolment,
-            programEncounter: programEncounter,
-            complicationsConcept: 'High Risk Conditions'
-        });
+//         return complicationsBuilder.getComplications();
+//     }
 
-      
-        const consanguineousMarriage = (programEncounter) => {
-            //return programEncounter.getObservationValue('Did you marry within relation?');
-           return programEnrolment.formElementGroup("Consanguineous marriage").value;
-        };
+//     static counselToDoctor(individual,today) {
+//         const complicationsBuilder = new ComplicationsBuilder({
+//             individual: individual,
+//             complicationsConcept: 'Age - less than 20'
+//         });
 
-        highRiskBuilder.addComplication("Consanguineous marriage")
-            .whenItem(consanguineousMarriage).is.equals("Yes");
+//         complicationsBuilder.addComplication("Counsel her to delay pregnancy by using contraceptives")
+//         .when.valueInRegistration("ageInMonths").lessThan(20);
+//        
+//             return complicationsBuilder.getComplications();
+//     }
 
-        let consanguineousMarriageHighRisk = highRiskBuilder.getComplications();
-        let highRiskConditions = lib.C.findValue(decisions['encounterDecisions'], 'High Risk Conditions');
-        [].push.apply(highRiskConditions,consanguineousMarriageHighRisk.value);
-
-
-return decisions;
-}
-}
+              
+//     static exec(individual, decisions, context, today) {
+//         decisions.registrationDecisions.push(PreconceptionRegistrationDecisions.consanguineousMarraige(individual));
+//         decisions.registrationDecisions.push(PreconceptionRegistrationDecisions.counselToDoctor(individual,today));
+//          return decisions;
+  
+//     }
+// }
 
 
-module.exports = {PreconceptionRegistrationViewHandler,PreconceptionRegistrationDecisions};
+module.exports = {PreconceptionRegistrationViewHandler};//PreconceptionRegistrationDecisions
 
