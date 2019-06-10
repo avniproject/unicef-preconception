36c1814d28143d8f05100f3e6edc3eab888a5876
sidtharthan <sidthunagarajan@gmail.com>
2019-06-10 15:51:31 +0530

    0000 | Try using create_db_user function to see if it works with different dbuser privilege levels

--- create_organisation.sql
+++ create_organisation.sql
@@ -1,10 +1,6 @@
-CREATE ROLE unicef_preconception_nashik NOINHERIT PASSWORD 'password';
-
-GRANT unicef_preconception_nashik TO openchs;
-
-GRANT ALL ON ALL TABLES IN SCHEMA public TO unicef_preconception_nashik;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO unicef_preconception_nashik;
-GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO unicef_preconception_nashik;
+select create_db_user('unicef_preconception_nashik', 'password');
 
 INSERT into organisation(name, db_user, uuid, parent_organisation_id)
-    SELECT 'UNICEF Preconception Nashik', 'unicef_preconception_nashik', '9f2f61e8-7559-4613-b490-f9b4f0eb5c03'  , id FROM organisation WHERE name = 'OpenCHS';
+values ('UNICEF Preconception Nashik', 'unicef_preconception_nashik', '9f2f61e8-7559-4613-b490-f9b4f0eb5c03',
+    (select id FROM organisation WHERE name = 'OpenCHS'))
+ON CONFLICT (uuid) DO NOTHING;
