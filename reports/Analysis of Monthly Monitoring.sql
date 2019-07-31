5e3461fa3fb07ecfb5b43e321a4a8757c36709d9
garimadosar5 <gmdosar@gmail.com>
2019-07-31 15:39:38 +0530

    Update Analysis of Monthly Monitoring.sql

--- reports/Analysis of Monthly Monitoring.sql
+++ reports/Analysis of Monthly Monitoring.sql
@@ -1,8 +1,8 @@
 Set role unicef_preconception_nashik;
 
 ----------------------------------------------------------------------------------------------------
--- 4. Check Early neonatal death/Late neonatal death shows up on the report.
---    Given an individual who has a pregnancy outcome of live birth and neonatal death, the Early/Late neonatal death count should increase by 1.
+-- 1. For Baseline and Visit 1 
+--    Given an individual's Baseline and Visit 1 is done, count should increase by 1.
 ----------------------------------------------------------------------------------------------------
 
 
